e5451847eb24f72bd57743e6536924693bbb8a52
Ilie Simion <iliecostin.simion@ixxus.com>
2016-03-28 15:41:45 +0300

    updated cards

--- src/main/java/com/ixxus/controller/QuizController.java
+++ src/main/java/com/ixxus/controller/QuizController.java
@@ -14,51 +14,20 @@ import org.springframework.web.bind.annotation.*;
 public class QuizController {
 
     @Autowired
-    PersonalityService personalityService;  //Service which will do all data retrieval/manipulation work
-
-//    private final Logger logger = LoggerFactory.getLogger(QuizController.class);
+    PersonalityService personalityService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Map<String, Object> model) {
-
-//        logger.debug("index() is executed!");
-
         model.put("title", ".getTitle");
         model.put("msg", "helloWorldService.getDesc()");
-
         return "index";
     }
-
-//    @RequestMapping(value = "/personality", method = RequestMethod.POST)
-//    public @ResponseBody String getMock(@RequestBody String json){
-//        return "POST" + json;
-//    }
-
-//    public String personality(Map<String, Object> model) {
-
-//        logger.debug("index() is executed!");
-
-//        model.put("title", ".getTitle");
-//        model.put("msg", "helloWorldService.getDesc()");
-//
-//        return "index";
-//    }
-//
-//    @RequestMapping(method = RequestMethod.GET, value = "/personality")
-//    public ResponseEntity<Person> deleteObject(@RequestBody Person id) {
-//        Person person = personService.findById(id);
-//        personService.deleteObject(id);
-//        return new ResponseEntity<Person>(person, HttpStatus.ACCEPTED);
-//    }
 
     @RequestMapping(method = RequestMethod.POST, value = "/personality")
     public @ResponseBody ResponseEntity<Personality> getPersonality(@RequestBody String answersJson) {
         Personality personality = personalityService.getPersonality(answersJson);
 
         return new ResponseEntity<Personality>(personality, HttpStatus.OK);
-
-//        String jsonData = "[{\"id\":\"3253123\",\"firstname\":\"Chris\",\"lastname\":\"Johnson\",\"address\":\"211, Geoffrey Drive\",\"city\":\"Newark\",\"phone\":\"999-888-6666\",\"email\":\"chrisj@yahoo.com\"},{\"id\":\"67643837\",\"firstname\":\"Bill\",\"lastname\":\"Derkson\",\"address\":\"201, Sleepy Hollow Drive\",\"city\":\"Newark\",\"phone\":\"999-777-2222\",\"email\":\"billd@gmail.com\"}]";
-//        return jsonData;
     }
 
 
--- src/main/java/com/ixxus/service/PersonalityService.java
+++ src/main/java/com/ixxus/service/PersonalityService.java
@@ -4,5 +4,5 @@ import com.ixxus.model.Personality;
 
 public interface PersonalityService {
 
-    public Personality getPersonality(String answers);
+    Personality getPersonality(String answers);
 }
